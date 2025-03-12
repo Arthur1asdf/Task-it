9d8bf7b3c37d85a0676f69b30b36748f9735dbfc
Arthur <arthurteng0819@gmail.com>
2025-03-11 23:58:39 -0400

    omg

--- largeProject/frontend/src/main.tsx
+++ largeProject/frontend/src/main.tsx
@@ -4,23 +4,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-<<<<<<< HEAD
     <h1>Hello, booty!</h1>
-=======
-    <h1>Hello, big asdfasdfasdfasfsad!</h1>
->>>>>>> d8e6a4c (testing for arianna)
-<<<<<<< Updated upstream
-=======
-=======
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Hello, big asdfasdfasdfasfsad!</h1>
-      <p>asldfjaslkjf</p>
-    </div>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   </StrictMode>
 );
