d67900b069ec76522848fb45d1b3a993bbd9b67a
Arthur <arthurteng0819@gmail.com>
2025-03-11 23:55:19 -0400

    hello

--- largeProject/frontend/src/main.tsx
+++ largeProject/frontend/src/main.tsx
@@ -4,6 +4,10 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+<<<<<<< HEAD
     <h1>Hello, booty!</h1>
+=======
+    <h1>Hello, big asdfasdfasdfasfsad!</h1>
+>>>>>>> d8e6a4c (testing for arianna)
   </StrictMode>
 );
